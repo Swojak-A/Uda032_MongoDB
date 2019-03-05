7ee84ae9e586fcd0599ebb39043b36bf43e87d14
kaoss <swojak.a@gmail.com>
2019-03-05 20:01:02 +0100

    add new twitter db and instructions

---
+++ mongo_04_macroimport_instructions.py
@@ -0,0 +1,11 @@
+"""
+
+how to import from a json file instructions:
+
+1. open gitbash
+2. go into the directory where is a file you want to import
+3. use command:
+mongoimport -d databaseName -c collectionName --filename fileName
+into gitbash console (not mongo console!)
+
+"""
