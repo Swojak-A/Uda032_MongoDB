5f916bcebcd670120938fb543f872da6279119df
kaoss <swojak.a@gmail.com>
2019-04-09 10:37:28 +0200

    fix mongo_04 - correcter import command

--- mongo_04_macroimport_instructions.py
+++ mongo_04_macroimport_instructions.py
@@ -5,7 +5,7 @@ how to import from a json file instructions:
 1. open gitbash
 2. go into the directory where is a file you want to import
 3. use command:
-mongoimport -d databaseName -c collectionName --filename fileName
+mongoimport -d databaseName -c collectionName --file fileName
 into gitbash console (not mongo console!)
 
 """
